a43375925b6e3a54ecefb62ddfe665a5e19bc514
KIKJUNW00 <kimjun9758@naver.com>
2024-11-13 14:26:39 +0900

    crud공부시작

---
+++ Misson1_API_List_Example/src/main/java/edu/pnu/Misson1ApiListExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.pnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Misson1ApiListExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Misson1ApiListExampleApplication.class, args);
+	}
+
+}
