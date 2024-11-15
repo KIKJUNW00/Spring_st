a3dcbd52839a967d7308471642602264201faf9c
KIKJUNW00 <kimjun9758@naver.com>
2024-11-15 14:27:50 +0900

    ㅇㅇ

---
+++ Mission4_DataBase/src/main/java/edu/pnu/Mission4DataBaseApplication.java
@@ -0,0 +1,13 @@
+package edu.pnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission4DataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mission4DataBaseApplication.class, args);
+	}
+
+}
